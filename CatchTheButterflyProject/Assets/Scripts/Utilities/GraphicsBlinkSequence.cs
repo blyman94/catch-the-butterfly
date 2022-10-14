f1465d239c7a25e76d9b2404aa990aef596ce3a4
Brandon Lyman <lyman.br@northeastern.edu>
2022-10-14 06:35:00 -0400

    Add graphics blink sequence
    + GraphicsBlinkSequence allows designer to toggle visibility of a mesh renderer over time, resulting in a "blinking" effect commonly used in games to show an actor has taken damage
    + Designer can choose the blink duration and and blink count to influence how dramatic the effect is
    + Added "Obstacle" layer and the "Ethereal" layer which will be used to toggle off obstacle collisions while the player is damaged
    + Used ProjectSettings to ensure the "Obstacle" and "Ethereal" layers cannot collide with each other

---
+++ CatchTheButterflyProject/Assets/Scripts/Utilities/GraphicsBlinkSequence.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Toggles the visibility of a MeshRenderer to create a flashing effect, a
+/// trope used to communicate that damage has been taken.
+/// </summary>
+public class GraphicsBlinkSequence : MonoBehaviour
+{
+    [SerializeField] private MeshRenderer _graphicsRenderer;
+    [SerializeField] private int _blinkCount;
+    [SerializeField] private float _blinkDuration;
+
+    private float _effectTimer;
+    private float _blinkTimer;
+    private float _timeBetweenBlinks;
+
+    #region MonoBehaviour Methods
+    private void Update()
+    {
+        if (_effectTimer > 0.0f)
+        {
+            if (_blinkTimer < 0.0f)
+            {
+                _graphicsRenderer.enabled = !_graphicsRenderer.enabled;
+                _blinkTimer = _timeBetweenBlinks;
+            }
+            _blinkTimer -= Time.deltaTime;
+            _effectTimer -= Time.deltaTime;
+            if (_effectTimer <= 0.0f)
+            {
+                _graphicsRenderer.enabled = true;
+            }
+        }
+    }
+    #endregion
+
+    /// <summary>
+    /// Sets the effect timer such that the blinking sequence begins. Calculates
+    /// time between blinks based on blink duration and blink count.
+    /// </summary>
+    public void StartSequence()
+    {
+        _effectTimer = _blinkDuration;
+        _timeBetweenBlinks = (_blinkDuration / _blinkCount) / 2.0f;
+        _blinkTimer = _timeBetweenBlinks;
+
+        _graphicsRenderer.enabled = false;
+    }
+}
