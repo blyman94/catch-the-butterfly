2dbdacf754476b807c114ac5d19c0221617c8ed8
Brandon Lyman <blyman94@comcast.net>
2022-10-17 19:15:43 -0400

    Add butterfly collision loop
    + Player can now enter the drown state by hitting a rock, and return from the drown state by jumping to collide with the butterfly
    + Add fake shadow directly below the player and the butterfly object. The isometric nature makes it very difficult to tell where the player is in relation to the butterfly. The shadow makes it obvious whether the player is directly below the butterfly
    + Edit tags to ensure the fake shadow feature worked properly on the level geometry

---
+++ CatchTheButterflyProject/Assets/Scripts/ShadowCaster.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+/// <summary>
+/// Uses a raycast to detect a surface below the caster. Then, moves a transform
+/// representing the shadow to the top of that surface. This fake shadow helps
+/// the player calculate where they should jump from when in isometric view.
+/// </summary>
+public class ShadowCaster : MonoBehaviour
+{
+    [SerializeField] private Transform _shadowTransform;
+    [SerializeField] private LayerMask _shadowCollisionLayerMask;
+
+    private void FixedUpdate()
+    {
+        RaycastHit info;
+        Physics.Raycast(transform.position,Vector3.down, out info, Mathf.Infinity, _shadowCollisionLayerMask);
+        _shadowTransform.localPosition = new Vector3(0.0f,-info.distance + 0.01f ,0.0f);
+    }
+}
