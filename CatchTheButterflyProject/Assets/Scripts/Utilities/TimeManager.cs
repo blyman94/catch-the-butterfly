469f306d100f3288c4746cbb462e329f63131206
Brandon Lyman <lyman.br@northeastern.edu>
2022-11-11 14:46:08 -0500

    Closed game loop
    + Add prototype start & end screens
    + [Tech Debt] Utilize Time.timeScale and SceneManagement to control game flow. This is a primitive technique and will need to be refactored in the future
    + Add goal to end of river, which will trigger the end game state

---
+++ CatchTheButterflyProject/Assets/Scripts/Utilities/TimeManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Time.timeScale = 0.0f;
+    }
+
+    public void StartTime()
+    {
+        Time.timeScale = 1.0f;
+    }
+    
+    public void StopTime()
+    {
+        Time.timeScale = 0.0f;
+    }
+}
