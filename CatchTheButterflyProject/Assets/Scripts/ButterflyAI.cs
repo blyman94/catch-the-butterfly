66b1c098d270028b10d7d0eff2606e5cb5f5151a
Brandon Lyman <lyman.br@northeastern.edu>
2022-11-16 00:52:59 -0500

    Butterfly spawn behaviour
    + A new butterfly will now spawn offscreen on a rock when the player enters the drown state. When the player gets close enough to the butterfly, it will take off
    + No more than one butterfly may be active at a time
    + The player can spawn as many butterflies as necessary
    + ButterflyAI parameterized to be tantalizing to the player
    + Color is restored at the end of the level

--- CatchTheButterflyProject/Assets/Scripts/ButterflyAI.cs
+++ CatchTheButterflyProject/Assets/Scripts/ButterflyAI.cs
@@ -5,45 +5,110 @@ public class ButterflyAI : MonoBehaviour
 {
     [SerializeField] private Vector3Variable _playerPosition;
     [SerializeField] private NavMeshAgent _navMeshAgent;
-    [SerializeField] private Vector2 _boundaryRange;
-    [SerializeField] private float _selectNewOffsetTime = 2.0f;
-    [SerializeField] private float _minHeight = 0.5f;
-    [SerializeField] private float _maxHeight = 2.0f;
+    [SerializeField] private Animator _animator;
     [SerializeField] private float _zTargetOffset = 5.0f;
+    [SerializeField] private float _startFlyingDistance = 2.0f;
+    
+    [Header("Random Ranges")]
+    [SerializeField] private Vector2 _boundaryRange;
+    [SerializeField] private Vector2 _baseOffsetRange;
+    [SerializeField] private Vector2 _speedRange;
+    
+    [Header("Random Position Intervals")]
+    [SerializeField] private float _selectNewXOffsetTime = 2.0f;
+    [SerializeField] private float _selectNewBaseOffsetTime = 2.0f;
+    [SerializeField] private float _selectNewSpeedTime = 2.0f;
+
+    private bool _isWaiting;
+    private int _flyTriggerHash;
 
     private float _xOffset;
-    private float _newOffsetTimer;
+    private float _newBaseOffsetTimer;
+    private float _newSpeedTimer;
     private float _lastBaseOffset;
     private float _newBaseOffset;
 
     #region MonoBehaviour Methods
+
+    private void Awake()
+    {
+        _flyTriggerHash = Animator.StringToHash("FlyTrigger");
+    }
     private void Start()
     {
-        _newOffsetTimer = _selectNewOffsetTime;
-
-        _navMeshAgent.baseOffset = _minHeight;
-        _lastBaseOffset = _navMeshAgent.baseOffset;
-        _newBaseOffset = _navMeshAgent.baseOffset;
+        _isWaiting = true;
+        _navMeshAgent.baseOffset = _baseOffsetRange.x;
     }
 
     private void Update()
     {
-        _navMeshAgent.SetDestination(new Vector3(_playerPosition.Value.x + _xOffset,
-            _playerPosition.Value.y, _playerPosition.Value.z + _zTargetOffset));
-
-        _newOffsetTimer -= Time.deltaTime;
-        _navMeshAgent.baseOffset = 
-            Mathf.Lerp(_lastBaseOffset, _newBaseOffset, 
-            1- (_newOffsetTimer / _selectNewOffsetTime));
-
-        if (_newOffsetTimer <= 0.0f)
+        if (_isWaiting)
         {
-            SelectRandomXOffset();
-            SelectRandomBaseOffset();
-            _newOffsetTimer = _selectNewOffsetTime;
+            if (Mathf.Abs(_playerPosition.Value.z - transform.position.z) <
+                _startFlyingDistance)
+            {
+                StartFlying();
+                _isWaiting = false;
+            }
+        }
+        else
+        {
+            // Decrement Timers
+            _newBaseOffsetTimer -= Time.deltaTime;
+        
+            // Calculate new base offset if necessary
+            if (_newBaseOffsetTimer <= 0.0f)
+            {
+                SelectRandomBaseOffset();
+                _newBaseOffsetTimer = _selectNewBaseOffsetTime;
+            }
+        
+            // Adjust base offset
+            _navMeshAgent.baseOffset = 
+                Mathf.Lerp(_lastBaseOffset, _newBaseOffset, 
+                    1- (_newBaseOffsetTimer / _selectNewBaseOffsetTime));
+        
+            float newPlayerPosX = _playerPosition.Value.x + _xOffset;
+            float newPlayerPosZ = _playerPosition.Value.z + _zTargetOffset;
+            _navMeshAgent.SetDestination(new Vector3(newPlayerPosX,
+                _playerPosition.Value.y, newPlayerPosZ));
         }
     }
     #endregion
+
+    private void StartFlying()
+    {
+        _navMeshAgent.speed = _speedRange.x;
+        
+        _newBaseOffsetTimer = _selectNewBaseOffsetTime;
+        _lastBaseOffset = _navMeshAgent.baseOffset;
+        _newBaseOffset = _lastBaseOffset;
+
+        InvokeRepeating(nameof(SelectRandomXOffset), 0.0f, 
+            _selectNewXOffsetTime);
+        InvokeRepeating(nameof(SelectRandomSpeed), 0.0f, 
+            _selectNewSpeedTime);
+        
+        _animator.SetTrigger(_flyTriggerHash);
+    }
+    
+    /// <summary>
+    /// Selects a random x offset to the player's position for the butterfly to 
+    /// travel to.
+    /// </summary>
+    private void SelectRandomBaseOffset()
+    {
+        _lastBaseOffset = _newBaseOffset;
+        _newBaseOffset = Random.Range(_baseOffsetRange.x, _baseOffsetRange.y);
+    }
+    
+    /// <summary>
+    /// Selects a random speed for the butterfly to travel at.
+    /// </summary>
+    private void SelectRandomSpeed()
+    {
+        _navMeshAgent.speed = Random.Range(_speedRange.x, _speedRange.y);
+    }
 
     /// <summary>
     /// Selects a random x offset to the player's position for the butterfly to 
@@ -55,15 +120,5 @@ public class ButterflyAI : MonoBehaviour
         float xOffsetMax = (Mathf.Abs(_playerPosition.Value.x - _boundaryRange.y));
 
         _xOffset = Random.Range(xOffsetMin, xOffsetMax);
-    }
-
-    /// <summary>
-    /// Selects a random x offset to the player's position for the butterfly to 
-    /// travel to.
-    /// </summary>
-    private void SelectRandomBaseOffset()
-    {
-        _lastBaseOffset = _newBaseOffset;
-        _newBaseOffset = Random.Range(_minHeight, _maxHeight);
     }
 }
